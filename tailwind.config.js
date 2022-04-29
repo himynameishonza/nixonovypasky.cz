7be628c29e25582a4a0c0859ee54f96cb0395bb9
Jan Rádl <radl.honza@gmail.com>
2022-04-29 17:49:01 +0200

    Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'PressStart2P': ['"Press Start 2P"', 'cursive'] // Ensure fonts with spaces have " " surrounding it.
+    },
     extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
